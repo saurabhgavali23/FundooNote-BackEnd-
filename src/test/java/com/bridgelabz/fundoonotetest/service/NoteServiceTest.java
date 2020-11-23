e93057ed654e8c1873aeddbd41f942415cb460c3
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-11-23 10:50:52 +0530

    [Saurabh] Add: Test case for note not save

--- src/test/java/com/bridgelabz/fundoonotetest/service/NoteServiceTest.java
+++ src/test/java/com/bridgelabz/fundoonotetest/service/NoteServiceTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.fundoonotetest.service;
 
 import com.bridgelabz.fundoonote.dto.NoteDTO;
+import com.bridgelabz.fundoonote.exception.FundooNoteException;
 import com.bridgelabz.fundoonote.service.NoteService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -30,5 +32,19 @@ public class NoteServiceTest {
         String message = noteServiceMock.SaveNote(any(), anyString());
 
         Assert.assertEquals("Note_Added_Successfully", message);
+    }
+
+    @Test
+    public void givenNoteDetails_whenNoteNotSave_thenThrowException(){
+
+        try{
+            when(noteServiceMock.SaveNote(any(),anyString())).thenThrow(new FundooNoteException("Note_Not_Save", HttpStatus.NOT_IMPLEMENTED.value()));
+
+            noteServiceMock.SaveNote(any(), anyString());
+
+        }catch (FundooNoteException n){
+
+            Assert.assertEquals(501, n.getHttpStatus());
+        }
     }
 }
