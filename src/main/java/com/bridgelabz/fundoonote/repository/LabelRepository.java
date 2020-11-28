76955009a65389197fa341d37ed51c6a75e1c073
saurabhgavali23 <gavalisaurabh02@gmail.com>
2020-11-28 14:42:05 +0530

    [Saurabh] Refactor: Label and note repository to add repository annotation

--- src/main/java/com/bridgelabz/fundoonote/repository/LabelRepository.java
+++ src/main/java/com/bridgelabz/fundoonote/repository/LabelRepository.java
@@ -4,11 +4,13 @@ import com.bridgelabz.fundoonote.module.LabelDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface LabelRepository extends JpaRepository<LabelDetails, Long> {
 
     @Transactional
--- src/main/java/com/bridgelabz/fundoonote/repository/NoteRepository.java
+++ src/main/java/com/bridgelabz/fundoonote/repository/NoteRepository.java
@@ -4,12 +4,14 @@ import com.bridgelabz.fundoonote.module.NoteDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface NoteRepository extends JpaRepository<NoteDetails, Long> {
 
     @Query(value = "SELECT * FROM note_details u WHERE u.user_id =?1", nativeQuery = true)
